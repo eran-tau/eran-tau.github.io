bc3048eae6c52b71ae2c608f82bf8ae9b38c382f
Eran Kirshenboim <kirshenboim1@mail.tau.ac.il>
2020-05-19 13:22:20 +0300

    inital commit

---
+++ precache-manifest.8ede2922995391aa9b7c6cbd41c9c5d0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c2f88c9f7c9a3544e700f8077b09c1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75944778b65f9b3fcf76",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "a5b136d1db6cd3785b95",
+    "url": "/static/css/main.0f13d19c.chunk.css"
+  },
+  {
+    "revision": "75944778b65f9b3fcf76",
+    "url": "/static/js/2.2a4164a4.chunk.js"
+  },
+  {
+    "revision": "6bfbeb5394052b98ea733dc8be25c7e8",
+    "url": "/static/js/2.2a4164a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5b136d1db6cd3785b95",
+    "url": "/static/js/main.c1081425.chunk.js"
+  },
+  {
+    "revision": "7269b0c4207b44d8a9e4f0b85c8a81d2",
+    "url": "/static/js/main.c1081425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeee28adb09e66de9efa",
+    "url": "/static/js/runtime-main.bdeca40f.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
